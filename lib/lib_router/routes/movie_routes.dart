fcb2800beef5c1f768e4448da67ca07245df4707
Stanislav Petkov <stanislav.petkov@primeholding.com>
2023-10-05 15:47:07 +0300

    Add two empty pages for movie and movie with details

---
+++ lib/lib_router/routes/movie_routes.dart
@@ -0,0 +1,36 @@
+part of '../router.dart';
+
+@immutable
+class MovieBranchData extends StatefulShellBranchData {
+  const MovieBranchData();
+}
+
+@immutable
+class MovieRoute extends GoRouteData implements RouteDataModel {
+  const MovieRoute();
+
+  @override
+  Page<Function> buildPage(BuildContext context, GoRouterState state) =>
+      MaterialPage(
+        key: state.pageKey,
+        child: const MoviePageWithDependencies(),
+      );
+
+  @override
+  String get routeLocation => location;
+}
+
+@immutable
+class MovieWithDetailsRoute extends GoRouteData implements RouteDataModel {
+  const MovieWithDetailsRoute();
+
+  @override
+  Page<Function> buildPage(BuildContext context, GoRouterState state) =>
+      MaterialPage(
+        key: state.pageKey,
+        child: const MovieWithDetailsPageWithDependencies(),
+      );
+
+  @override
+  String get routeLocation => location;
+}
