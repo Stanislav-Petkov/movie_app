b646fe6e1c5b759db0777c58356e7c766b145d45
Stanislav Petkov <stanislav.petkov@primeholding.com>
2023-10-07 10:29:46 +0300

    Handle SocketException in ui

--- lib/feature_movie/ui_components/movie_image.dart
+++ lib/feature_movie/ui_components/movie_image.dart
@@ -1,8 +1,13 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 
+import '../../app_extensions.dart';
+import '../../base/common_ui_components/app_error_widget.dart';
+import '../../base/models/errors/error_model.dart';
 import '../../base/models/movie_model.dart';
 
-class MovieImage extends StatelessWidget {
+class MovieImage extends StatefulWidget {
   const MovieImage({
     required this.movie,
     super.key,
@@ -11,13 +16,18 @@ class MovieImage extends StatelessWidget {
   final MovieModel movie;
 
   @override
+  State<MovieImage> createState() => _MovieImageState();
+}
+
+class _MovieImageState extends State<MovieImage> {
+  @override
   Widget build(BuildContext context) => Hero(
-        tag: 'MovieImage${movie.id}',
+        tag: 'MovieImage${widget.movie.id}',
         child: SizedBox(
           height: 300,
           width: 200,
           child: Image.network(
-            'https://image.tmdb.org/t/p/w500${movie.posterPath}?api_key=9832af988b9228550b81b98fee2efec8',
+            'https://image.tmdb.org/t/p/w500${widget.movie.posterPath}?api_key=9832af988b9228550b81b98fee2efec8',
             fit: BoxFit.cover,
             loadingBuilder: (context, child, loadingProgress) {
               if (loadingProgress == null) {
@@ -26,6 +36,18 @@ class MovieImage extends StatelessWidget {
               return const Center(
                 child: CircularProgressIndicator(),
               );
+            },
+            errorBuilder: (context, error, stackTrace) {
+              if (error is SocketException) {
+                return Center(child: Text(context.l10n.error.network));
+              } else if (error is ErrorModel) {
+                return AppErrorWidget(
+                  error: error,
+                  onTabRetry: () {},
+                );
+              } else {
+                return Text(context.l10n.error.unknown);
+              }
             },
           ),
         ),
